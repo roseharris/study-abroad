ee25d23781ae3a31f3e260b1646254b197b3213b
roseharris <roseharris801@gmail.com>
2021-11-16 09:56:19 -0700

    Media Breakpoints Added

---
+++ node_modules/bulma-calendar/src/js/datePicker/defaultOptions.js
@@ -0,0 +1,19 @@
+const defaultOptions = {
+  color: 'primary',
+  isRange: false,
+  allowSameDayRange: true,
+  lang: 'en', // internationalization
+  startDate: undefined,
+  endDate: undefined,
+  minDate: null,
+  maxDate: null,
+  disabledDates: [],
+  disabledWeekDays: undefined,
+  weekStart: 0,
+  dateFormat: 'MM/DD/YYYY',
+  weekDaysFormat: 'ddd',
+  enableMonthSwitch: true,
+  enableYearSwitch: true
+};
+
+export default defaultOptions;
